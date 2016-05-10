3d97e3d5424ef234050035ae2d7460fd2237aa0d
lawton and veronica <lawton.a.browning@gmail.com>
2016-05-10 09:23:20 -0700

    finish wiki game

--- src/WikiGame/wwwroot/js/Scripts.js
+++ src/WikiGame/wwwroot/js/Scripts.js
@@ -5,51 +5,80 @@
     var search = " ";
     var searchUrl = 'https://en.wikipedia.org/w/api.php';
 
+    var clicks = 0;
+
     button.click(function () {
         articles.empty();
-        search = input.val();
+        //search = input.val();
         ajaxArticleData();
+        $('.goaltitle').append(input.val());
+        clicks++;
     });
+
+  
+
+    var ClickCounterViewModel = function () {
+        this.numberOfClicks = ko.observable(0);
+
+        this.registerClick = function () {
+            this.numberOfClicks(this.numberOfClicks() + 1);
+        };
+    };
+
+    ko.applyBindings(new ClickCounterViewModel());
 
     $('body').on('click', '.link', function () {
         console.log(this);
-        var textLink = document.getElementById(this);
-        search = textLink.innerHtml;
+        var textLink = $(this).text()
+        console.log(textLink);
+        search = textLink
+        $(".articles").empty();
         ajaxArticleData();
     });
 
     var ajaxArticleData = function () {
-        $.ajax({
-            type: 'GET',
-            dataType: 'jsonp',
-            url: searchUrl,
-            data: {
-                action: 'query',
-                format: 'json',
-                prop: 'links',
-                pllimit: 500,
-                titles: search,
-                
+        if (clicks == 0) {
+            var generatorType = 'random';
+        } else {
+            generatorType = 'search';
+        }
+   
+            $.ajax({
+                type: 'GET',
+                dataType: 'jsonp',
+                url: searchUrl,
+                data: {
+                    action: 'query',
+                    format: 'json',
+                    prop: 'links',
+                    pllimit: "max",
+                    //titles: search,
+                    generator: generatorType,
+                    gsrsearch: search,
+                    grnnamespace: 0,
 
-            },
-            success: function (json) {
-                var pages = json.query.pages;
-                $.map(pages, function (page) {
-                    var pageElement = $('<div>');
 
-                    //get the article title
-                    pageElement.append($('<h2>').text(page.title));
 
-                    $.map(page.links, function (link) {
+                },
+                success: function (json) {
+                    var pages = json.query.pages;
+                    //for each pages call it page then do{x}
+                    $.map(pages, function (page) {
+                        var pageElement = $('<div>');
 
-                        $('.articles').append($('<span class="link">' + link.title + '</span>'));
+                        //get the article title
+                        $('.articles').append($('<h2>').text(page.title));
+
+                        $.map(page.links, function (link) {
+
+                            $('.articles').append($('<p><span class="link">' + link.title + '</span></p>'));
+                        });
+
+                        pageElement.append($('<hr>'));
+
+                        articles.append(pageElement);
                     });
-
-                    pageElement.append($('<hr>'));
-
-                    articles.append(pageElement);
-                });
-            }
-        })
+                }
+            })
     };
 });
